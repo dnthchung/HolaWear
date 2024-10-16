d09c025a16ca967d42861d2e4d9c92eb601bcd32
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-16 15:01:57 +0700

    Refactor import statements in ShippingAddress component

--- frontend/src/components/auth/ShippingAddress.jsx
+++ frontend/src/components/auth/ShippingAddress.jsx
@@ -4,17 +4,7 @@ import { toast } from "react-hot-toast";
 import { UserContext } from "@/App";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
